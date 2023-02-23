f15c6c1d7a8b30b6db39faebaf8736d5beb47861
Dmitry Vorobyev <d.vorobev@mxmgt.com>
2023-02-23 16:57:48 +0300

    Resolve four sum task

--- Leetcode.Services/FourSumService.cs
+++ Leetcode.Services/FourSumService.cs
@@ -1,4 +1,7 @@
-﻿namespace Leetcode.Services;
+﻿using BenchmarkDotNet.Environments;
+using System.Numerics;
+
+namespace Leetcode.Services;
 
 public class FourSumService
 {
@@ -18,10 +21,90 @@ public class FourSumService
        Input: nums = [2,2,2,2,2], target = 8
        Output: [[2,2,2,2]]
      */
+
     public static IList<IList<int>> GetUniqueQuadruplets(int[] nums, int target)
     {
-        var result = new List<IList<int>>();
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (dict.TryAdd(target - nums[i], i)) ;
+        }
+
+        var h = new HashSet<(int, int, int, int)>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                for (int k = j + 1; k < nums.Length; k++)
+                {
+                    if (i != j && i != k && k != j && dict.TryGetValue(nums[i] + nums[j] + nums[k], out int keyValK))
+                    {
+                        if (keyValK == i || keyValK == j || keyValK == k)
+                            continue;
+
+                        try
+                        {
+                            checked
+                            {
+                                var t = nums[i] + nums[j] + nums[k];
+                                var r = new[] { target - t, nums[i], nums[j], nums[k] };
+                                Array.Sort(r);
+                                h.Add((r[0], r[1], r[2], r[3]));
+                            }
+                        }
+                        catch (OverflowException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        var result = (IList<IList<int>>)h.Select(x => (IList<int>)new List<int> { x.Item1, x.Item2, x.Item3, x.Item4 }).ToList();
 
         return result;
+    }
+
+    public static IList<IList<int>> GetUniqueQuadrupletsV1(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            dict.Add(i, target - nums[i]);
+        }
+
+        var h = new HashSet<(int, int, int, int)>();
+        KeVal x = new();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                for (int k = i + 1; k < nums.Length; k++)
+                {
+                    if (i != j && i != k && k != j && dict.Any(x => x.Value == nums[i] + nums[j] + nums[k]))
+                    {
+                        x.K = dict.First(x => x.Value == nums[i] + nums[j] + nums[k]);
+                        if (x.K.Key == i || x.K.Key == j || x.K.Key == k)
+                            continue;
+
+                        var r = new[] { target - nums[i] - nums[j] - nums[k], nums[i], nums[j], nums[k] };
+                        Array.Sort(r);
+                        h.Add((r[0], r[1], r[2], r[3]));
+                    }
+                }
+            }
+        }
+
+        var result = (IList<IList<int>>)h.Select(x => (IList<int>)new List<int> { x.Item1, x.Item2, x.Item3, x.Item4 }).ToList();
+
+        return result;
+    }
+
+    public class KeVal
+    {
+        public KeyValuePair<int, int> K { get; set; }
     }
 }
--- Leetcode.Tests.Unit/FourSumServiceTest.cs
+++ Leetcode.Tests.Unit/FourSumServiceTest.cs
@@ -1,5 +1,4 @@
 ﻿using Leetcode.Services;
-using System.Diagnostics;
 
 namespace Leetcode.Tests.Unit;
 
@@ -19,27 +18,35 @@ Output: [[2,2,2,2]]
     [DynamicData(nameof(CombinationTests), DynamicDataSourceType.Method)]
     public void FourSum(TestData testData)
     {
-        var result = FourSumService.GetUniqueQuadruplets(testData.nums, testData.target);
-
-        Assert.AreEqual(testData.expected, result);
+        var result = FourSumService.GetUniqueQuadruplets(testData.Nums, testData.Target);
+        var r = result as List<IList<int>>;
+        var t = testData.Expected.SelectMany(x => x).ToArray();
+        CollectionAssert.AreEqual(
+            testData.Expected.SelectMany(x => x).ToArray(),
+            r.SelectMany(x => x).ToArray());
+        Assert.AreEqual(true, true);
     }
 
     public class TestData
     {
-        public int[][] expected { get; set; }
-        public int target { get; set; }
-        public int[] nums { get; set; }
+        public List<int[]> Expected1 { get; set; }
+        public List<IList<int>> Expected { get; set; }
+        public int Target { get; set; }
+        public int[] Nums { get; set; }
 
-        public TestData(int[][] expected, int target, int[] nums)
+        public TestData(List<IList<int>> expected, int target, int[] nums)
         {
-            this.expected = expected;
-            this.target = target;
-            this.nums = nums;
+            this.Expected = expected;
+            this.Target = target;
+            this.Nums = nums;
         }
     }
 
     public static IEnumerable<TestData[]> CombinationTests()
     {
-        yield return new TestData[] { new TestData(new int[][] { new[] { -2, -1, 1, 2 }, new[] { -2, -1, 1, 2 } }, 8, new[] { 2, 2, 2, 2, 2 }) };
+        // yield return new TestData[] {new TestData(new List<IList<int>> {new[] { -2, -1, 1, 2 }, new[] { -2, 0, 0, 2 }, new[] { -1, 0, 0, 1 }}, 0, new[] { 1, 0, -1, 0, -2, 2 })};
+        // yield return new TestData[] { new TestData(new List<IList<int>> { new[] { -2, -1, 1, 2 } }, 1896, new[] { -490, -481, -471, -467, -453, -450, -446, -440, -437, -424, -423, -391, -384, -373, -358, -332, -318, -314, -311, -309, -299, -279, -270, -257, -243, -208, -205, -171, -153, -147, -142, -138, -129, -99, -20, -19, 17, 30, 44, 82, 86, 93, 122, 125, 138, 139, 158, 169, 175, 181, 199, 200, 203, 203, 205, 225, 248, 272, 277, 306, 334, 335, 337, 338, 342, 344, 359, 396, 403, 405, 412, 434, 437, 439, 440, 441, 443, 446, 446, 457, 465, 468, 473, 496 }) };
+        // yield return new TestData[] { new TestData(new List<IList<int>> { new[] { 2, 2, 2, 2 } }, 8, new[] { 2, 2, 2, 2, 2 }) };
+        yield return new TestData[] { new TestData(new List<IList<int>> { }, -294967296, new[] { 1000000000, 1000000000, 1000000000, 1000000000 }) };
     }
 }
