e4cd99e43e72892a5b04d132c136f3f1c51f7ae3
dmitrii vorobev <dmitrii.vorobev@redlab.dev>
2024-02-25 08:24:08 +0300

    Add new samples for Trap service

--- Leetcode.Services/TrapService.cs
+++ Leetcode.Services/TrapService.cs
@@ -4,7 +4,7 @@
 /// https://leetcode.com/problems/trapping-rain-water/
 /// Улавливание дождевой воды
 /// </summary>
-public static class TrapService
+public class TrapService
 {
     /// <summary>
     /// [0,1,0,2,1,0,1,3,2,1,2,1] -> 6
@@ -12,29 +12,87 @@ public static class TrapService
     /// </summary>
     /// <param name="height"></param>
     /// <returns></returns>
-    public static int Trap(int[] height)
+    public int Trap(int[] height)
     {
-        var currentMaxValue = 0;
-        var isCounting = false;
-        var prevHeight = 0;
-        var currentHeigts = new List<int>();
-        var volumes = new List<int>();
+        var volume = 0;
+        var cicle = 100;
+        CalculateVolume(height, ref volume, ref cicle);
 
+        return volume;
+    }
+
+    private int CalculateVolume(int[] height, ref int volume, ref int cicle)
+    {
+        cicle--;
+        if (cicle < 0 || height.Length < 3)
+        {
+            return volume;
+        }
+
+        var prevHeight = 0;
+        var currentMaxValue = 0;
+        int i = 0;
+        var isCounting = false;
+        var isLeft = true;
+        var isFirst = true;
+        int currentVolume = 0;
+        var currentHeigts = new List<int>();
+        var isFirstAlreadyHere = false;
         foreach (var heightItem in height)
         {
+            i++;
             // если встретился 0 и сейчас сбит флаг подсчета
-            if (heightItem == 0 && !isCounting)
+            if (heightItem == 0 && !isCounting) //  || prevHeight == heightItem)
             {
                 continue;
             }
 
-            currentHeigts.Add(heightItem);
-
-            if (currentMaxValue > heightItem && prevHeight < heightItem)
+            // когда идет увеличение, но еще нет подсчета
+            if (!isCounting && prevHeight < heightItem)
             {
-                volumes.Add(CalculateVolume());
+                continue;
+            }
+
+            // если мы еще только хотим считать,
+            // но уже понимаем, что текущее значение меньше предыдущего
+            if (isCounting && !currentHeigts.Any() && prevHeight < heightItem)
+            {
                 isCounting = false;
-                currentMaxValue = heightItem; 
+                isFirst = true;
+                currentVolume = 0;
+                continue;
+            }
+
+            // если мы имеем дело с левой границей емкости
+            if ((!isFirst && currentMaxValue > heightItem) || prevHeight > heightItem)
+            {
+                if (i != height.Length)
+                    currentVolume += currentMaxValue - heightItem;
+
+                currentHeigts.Add(heightItem);
+            }
+
+            // если мы имеем дело с левой границей емкости и она равна следующему значению, то можно ее убрать
+            if (!isFirst && currentHeigts.Count == 0 && prevHeight == heightItem)
+            {
+                continue;
+            }
+
+            // в случае если правая сторона больше или равна левой (левая как бы считается максимальной)
+            // и проверяем чтобы текущая точно была больше предыдущей
+            if (isCounting && currentMaxValue <= heightItem && prevHeight < heightItem)
+            {
+                volume += currentVolume;
+                currentVolume = 0;
+                isCounting = false;
+                isFirst = true;
+                currentMaxValue = heightItem;
+                prevHeight = currentMaxValue;
+                currentHeigts = new List<int>()
+                {
+                    heightItem
+                };
+                isFirstAlreadyHere = true;
                 continue;
             }
 
@@ -43,17 +101,50 @@ public static class TrapService
                 currentMaxValue = heightItem;
             }
 
+            // если счетчик не включен еще, то получается, что уже первый прошел
+            if (!isCounting && isFirst)
+            {
+                isFirst = false;
+            }
+
             isCounting = true;
             prevHeight = heightItem;
+
+            // если мы на последнем шаге входного массива
+            // и левая граница больше текущей, то 
+            // нужен пересчет или заново запустить CalculateVolume()
+            // с новыми параметрами
+            if (i == height.Length && currentMaxValue > heightItem)
+            {
+                if (currentMaxValue != heightItem && !isFirstAlreadyHere)
+                {
+                    currentHeigts.Insert(0, heightItem);
+                    isFirstAlreadyHere = false;
+                }
+
+                if (currentHeigts.Count < 3)
+                {
+                    isLeft = false;
+                    break;
+                }
+
+                isLeft = true;
+                var a = currentHeigts[0];
+                var b = currentHeigts[^1];
+
+                if (a > b)
+                    currentHeigts[0] = a - 1;
+
+                break;
+            }
         }
 
-        return 0;
-    }
+        if (isLeft)
+        {
+            return CalculateVolume(currentHeigts.ToArray(), ref volume, ref cicle);
+        }
 
-    private static int CalculateVolume()
-    {
-        // currentHeigts
-        return 0;
+        return volume;
     }
 }
 
--- Leetcode.Tests.Unit/TrapServiceTests.cs
+++ Leetcode.Tests.Unit/TrapServiceTests.cs
@@ -6,11 +6,14 @@ namespace Leetcode.Tests.Unit;
 public class TrapServiceTests
 {
     [TestMethod]
-    // [DataRow(6, new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 })]
-    [DataRow(9, new[] { 4, 2, 0, 3, 2, 5 })]
+    //[DataRow(6, new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 })]
+    //[DataRow(9, new[] { 4, 2, 0, 3, 2, 5 })]
+    //[DataRow(1, new[] { 4, 2, 3 })]
+    //[DataRow(7, new[] { 0, 7, 1, 4, 6 })]
+    [DataRow(26, new[] { 0, 1, 2, 0, 3, 0, 1, 2, 0, 0, 4, 2, 1, 2, 5, 0, 1, 2, 0, 2 })]
     public void Trap(int expected, int[] nums)
     {
-        var result = TrapService.Trap(nums);
+        var result = new TrapService().Trap(nums);
 
         Assert.AreEqual(expected, result);
     }
